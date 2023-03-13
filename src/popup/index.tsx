486885458d3993faab9555fad6c14b1a3e2dbaf0
Ruoyu <klaus1201810802@gmail.com>
2023-03-13 22:30:31 +0800

    💄 add style and icons

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react"
 
+// import BoxIn from "~/icons/BoxIn"
+import CopyIcon from "~/icons/Copy"
+import ObsidianIcon from "~icons/obsidian"
 import type { Article, Message } from "~types"
 
-import "../style/base.css"
+import "~/style/base.css"
 
 function IndexPopup() {
-  const [content, setParsedContent] = useState<any>({})
+  const [content, setParsedContent] = useState<Article>()
   const [article, setArticle] = useState<string>()
+  const [showCopied, setShowCopied] = useState(false)
 
   useEffect(() => {
     const parsedContentListener = (message: Message<Article>) => {
@@ -28,22 +32,77 @@ function IndexPopup() {
 
   const copyToClipboard = () => {
     navigator.clipboard.writeText(article)
+    setShowCopied(true)
   }
+
+  const copyToObsidian = () => {
+    document.location.href =
+      "obsidian://new?" +
+      "file=" +
+      encodeURIComponent(" " + content.title) +
+      "&content=" +
+      encodeURIComponent(article)
+  }
+
+  useEffect(() => {
+    if (showCopied) {
+      setTimeout(() => {
+        setShowCopied(false)
+      }, 2000)
+    }
+  }, [showCopied])
 
   return (
     <div
-      style={{
-        width: "400px",
-        height: "500px",
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <textarea
-        style={{ height: 500, resize: "none" }}
-        value={article}
-        onChange={(e) => setArticle(e.target.value)}></textarea>
-      <button onClick={copyToClipboard}>Copy To Clipboard</button>
+      className="container flex flex-col p-3 border-8 border-darkPurple bg-lightPurple "
+      style={{ width: 720, height: 560 }}>
+      <h1 className="leading-normal text-4xl text-center font-bold text-white">
+        Obsidian Clipper
+      </h1>
+
+      <div className="flex flex-col w-full h-full mt-3 pb-2 ">
+        <div className="flex flex-row mb-4 justify-between">
+          <div>
+            {/* <input
+              className="mr-4 p-1 shadow-lg"
+              style={{ width: 160, height: 36 }}
+              type="text"
+              placeholder="Vault Folder eg. /resource"
+            />
+            <input
+              className="p-1 shadow-lg"
+              style={{ width: 160, height: 36 }}
+              type="text"
+              placeholder="Tags eg. #web"
+            /> */}
+          </div>
+          <div>
+            {/* <button
+              style={{ width: 36, height: 36 }}
+              className="mr-4 bg-white border border-grey500 shadow text-center"
+              onClick={copyToClipboard}>
+              <BoxIn />
+            </button> */}
+            <button
+              style={{ width: 36, height: 36 }}
+              className="mr-4 bg-white border border-grey500 shadow"
+              onClick={copyToClipboard}>
+              <CopyIcon />
+            </button>
+            <button
+              style={{ width: 36, height: 36 }}
+              className="bg-white border border-grey500 shadow"
+              onClick={copyToObsidian}>
+              <ObsidianIcon />
+            </button>
+            {showCopied && <p className="absolute">Copied!</p>}
+          </div>
+        </div>
+        <textarea
+          className="h-full resize-none p-2 rounded"
+          value={article}
+          onChange={(e) => setArticle(e.target.value)}></textarea>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{tsx}"],
+  content: ["./src/**/*.{html,tsx,ts}"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        darkPurple: "#443C93",
+        lightPurple: "#A9A2E8",
+        pink: "#F6D2D2"
+      }
+    }
   },
   plugins: []
 }
